17efc01f923fce0476f0318843b6fa6cec9b5f0a
Steven Tew <steve.tew@musicmagpie.co.uk>
2021-06-03 14:08:03 +0100

    Add SVG content and change file names

--- packages/wireframe-ui/src/components/ComponentExample4/index.js
+++ packages/wireframe-ui/src/components/ComponentExample4/index.js
@@ -1,13 +1,13 @@
 // import React from 'react';
 import PropTypes from "prop-types";
 import css from './ComponentExample4.module.scss';
-import image from './empty.svg';
+import image from './example.svg';
 
 const ComponentExample4 = () => {
   return (
     <div className={css.ComponentExample4}>
       <h2 className={css.ComponentExample4__heading}>ComponentExample4</h2>
-      <img src={image} alt="" />
+      <img src={image} alt="svg circle" />
     </div>
   );
 };
